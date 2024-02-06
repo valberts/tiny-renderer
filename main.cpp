43552a77da35bd0531a36eb53c7983e7b7628ca9
valberts <albertsson.vincent@gmail.com>
2024-02-06 18:07:19 +0100

    Drawing triangles successfully

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
 
     for (int x = p0.x; x <= p1.x; x++) {
         float t = (x - p0.x) / (float)(p1.x - p0.x);
-        int y = p0.y * (1. - t) + p1.y * t;
+        int y = p0.y * (1.0f - t) + p1.y * t;
         if (steep) {
             image.set(y, x, color);
         }
@@ -33,50 +33,27 @@ void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
         }
     }
 }
-// t2 > t1 and t1 > t0 
-// t0, t1, t2 --> t2, t1, t0
-
-// t2 > t1 and t0 > t1
-// t0, t1, t2 --> t2, t0, t1
-
-// t1 > t2 and t1 > t0
-// t0, t1, t2 --> t1, t0, t2
-
-// t1 > t2 and t1 < t0
-// t0, t1, t2 --> t0, t1, t2
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
-    if (t2.y > t1.y) {
-        if (t1.y > t0.y) {
-            std::swap(t2, t0);
-        }
-        else {
-            std::swap(t2, t0);
-            std::swap(t2, t1);
-        }
-    } else {
-        if (t1.y > t0.y) {
-            std::swap(t1, t0);
-        }
-        else {
-            // leave it as is
+    if (t0.y == t1.y && t0.y == t2.y) return; // ignore degenerate triangles
+    // sort vertices, t0, t1, t2, lower-to-upper
+    if (t0.y > t1.y) std::swap(t0, t1);
+    if (t0.y > t2.y) std::swap(t0, t2);
+    if (t1.y > t2.y) std::swap(t1, t2);
+
+    int total_height = t2.y - t0.y;
+    for (int i = 0; i < total_height; i++) {
+        bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+        int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+        float alpha = (float)i / total_height;
+        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
+        Vec2i A = t0 + (t2 - t0) * alpha;
+        Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+        if (A.x > B.x) std::swap(A, B);
+        for (int x = A.x; x <= B.x; x++) {
+            image.set(x, t0.y + i, color);
         }
     }
-    line(t0, t1, image, white);
-    line(t1, t2, image, red);
-    line(t2, t0, image, green);
-
-    int top = t0.y;
-    int bottom = t2.y;
-    int height = top - bottom;
-
-    int width = std::abs(t0.x - t2.x);
-
-    for (int i = top; i > height; --i) {
-        for (int j = t0.x; j < t2.x; ++j) {
-            image.set(j,i, white);
-        }
-    }
-
 }
 
 int main(int argc, char** argv) {

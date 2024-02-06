e14a53248616b836a9ddd8515d33998f80b2a22f
Vincent Albertsson <albertsson.vincent@gmail.com>
2024-02-06 22:27:13 +0100

    Drawing a 3d model with triangles and light intensity based on normal

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
-#include <vector>
 #include <cmath>
-#include "tgaimage.h"
-#include "model.h"
+#include <cstdlib>
+#include <limits>
+#include <vector>
+
 #include "geometry.h"
+#include "model.h"
+#include "tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 Model* model = NULL;
-const int width = 200;
-const int height = 200;
+const int width = 800;
+const int height = 800;
 
 void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
     bool steep = false;
@@ -27,14 +30,14 @@ void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color) {
         int y = p0.y * (1.0f - t) + p1.y * t;
         if (steep) {
             image.set(y, x, color);
-        }
-        else {
+        } else {
             image.set(x, y, color);
         }
     }
 }
+
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
-    if (t0.y == t1.y && t0.y == t2.y) return; // ignore degenerate triangles
+    if (t0.y == t1.y && t0.y == t2.y) return;  // ignore degenerate triangles
     // sort vertices, t0, t1, t2, lower-to-upper
     if (t0.y > t1.y) std::swap(t0, t1);
     if (t0.y > t2.y) std::swap(t0, t2);
@@ -45,7 +48,8 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
         bool second_half = i > t1.y - t0.y || t1.y == t0.y;
         int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
         float alpha = (float)i / total_height;
-        float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
+        float beta =
+            (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
         Vec2i A = t0 + (t2 - t0) * alpha;
         Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
 
@@ -57,35 +61,40 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-    //if (2 == argc) {
-    //    model = new Model(argv[1]);
-    //}
-    //else {
-    //    model = new Model("obj/african_head.obj");
-    //}
+    if (2 == argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
 
     TGAImage image(width, height, TGAImage::RGB);
-  //  for (int i = 0; i < model->nfaces(); i++) {
-  //      std::vector<int> face = model->face(i);
-		//for (int j = 0; j < 3; j++) {
-  //          Vec3f v0 = model->vert(face[j]);
-  //          Vec3f v1 = model->vert(face[(j + 1) % 3]);
-  //          int x0 = (v0.x + 1.0f) * width / 2.0f;
-  //          int y0 = (v0.y + 1.0f) * height / 2.0f;
-  //          int x1 = (v1.x + 1.0f) * width / 2.0f;
-  //          int y1 = (v1.y + 1.0f) * height / 2.0f;
-  //          line(x0, y0, x1, y1, image, white);
-  //      }
-  //  }
 
-    Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-    Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+    Vec3f light_dir(0, 0, -1);  // define light_dir
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    for (int i = 0; i < model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j = 0; j < 3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] =
+                Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            world_coords[j] = v;
+        }
+        Vec3f n = (world_coords[2] - world_coords[0]) ^
+                  (world_coords[1] - world_coords[0]);
+        n.normalize();
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2],
+                     image,
+                     TGAColor(intensity * 255, intensity * 255, intensity * 255,
+                              255));
+        }
+    }
+
+    image.flip_vertically();  // i want to have the origin at the left
+                              // bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
     return 0;
